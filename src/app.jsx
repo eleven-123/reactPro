bfe7c772c42dbae29909b19763526a7a8c11fcc4
eleven_威 <1782981957@qq.com>
2020-11-06 15:41:24 +0800

    'init'

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,5 @@
-import React from 'react'
-import Hello from './components/funComponent.jsx';
-var person = {
-  name:'zs',
-  age:18
-}
-
-/* 类组件 */
-/* class DivCompoent extends React.Component{
-  render(){
-    return (
-      <div>
-        <h2>这是classComponent </h2>
-        <p>{this.props.name}- {this.props.age}</p>
-      </div>
-    );
-  }
-} */
-import DivCompoent from './components/classComponent.jsx';
-import Comment from './components/comment/comment.jsx';
-import Counter from './components/counter/index.jsx';
-import { DatePicker } from 'antd';
-
-import { HashRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import React from 'react';
+import { HashRouter as Router, Route, Link} from 'react-router-dom';
 
 export default class App extends React.Component{
   constructor(){
@@ -30,18 +8,7 @@ export default class App extends React.Component{
   render(){
     return (
       <Router>
-        <Link to="/comment">Link A</Link>
-        <Link to="/count">Link B</Link>
-        <DatePicker />
-
-        {/* 匹配规则 */}
-        <Route path="/comment" component={Comment} />
-        {/* <Route path="/comment">
-          <Comment />
-        </Route> */}
-        <Route path="/count">
-          <Counter />
-        </Route>
+        
       </Router>
     )
   }
--- src/components/comment/commentItem.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './style.scss'
-
-export default function Comment(props){
-  return(
-    <div className={styles.box} > {props.user} - {props.content}</div>
-  )
-}
